1c572d6583e13357d5a04be31494e263c85c7954
stokely <adrienne.j.abrams@gmail.com>
2019-02-25 18:24:32 -0600

    Updates

---
+++ precache-manifest.d8d3744ed6222aa415ce2160b64d75eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdabbf2a4db86b64c3ba",
+    "url": "/finalClicky/static/js/runtime~main.fdabbf2a.js"
+  },
+  {
+    "revision": "00e220e2dcb827fd15d4",
+    "url": "/finalClicky/static/js/main.00e220e2.chunk.js"
+  },
+  {
+    "revision": "53341b06a5b8e5594afa",
+    "url": "/finalClicky/static/js/1.53341b06.chunk.js"
+  },
+  {
+    "revision": "00e220e2dcb827fd15d4",
+    "url": "/finalClicky/static/css/main.c87e21e0.chunk.css"
+  },
+  {
+    "revision": "f8a4e283014779cebed4ab8252c2a57f",
+    "url": "/finalClicky/index.html"
+  }
+];
